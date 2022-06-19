bcce6f16226ec7a270a9a6cb635977f46fd08ed3
deBabbbe <neotbh@gmx.de>
2022-06-19 21:45:59 +0200

    Added serverless function for vercel

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ const server = http.createServer((_, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+export default function handler(_, res) {
+  return res.send(configs);
+}
